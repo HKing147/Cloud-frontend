3de33fb8f428dfb51841389dd2943008aa2c2964
HKing <1470042308@qq.com>
2023-03-05 21:31:12 +0800

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,6 @@ import TheWelcome from "./components/TheWelcome.vue";
 <template>
 	<header>
 		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-		HelloWorld
-
 		<div class="wrapper">
 			<HelloWorld msg="You did it!" />
 		</div>
