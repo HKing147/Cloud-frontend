aa8f1955925a98a7bc715ea21a97e701444ec3f6
HKing <1470042308@qq.com>
2023-03-12 23:52:38 +0800

    modified:   src/apis/index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,8 +2,22 @@
 import axios from "axios";
 import service from "../request/index.js";
 
+// 上传小文件
 export function uploadFile(file, url) {
 	service.post(url, { file: file }, { headers: { "Content-Type": "multipart/form-data" } });
+	// service.post(url, { file: file }, options)
+	// 	.then((response) => {
+	// 		console.log(response);
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	});
+}
+
+// 上传大文件(分片上传)
+export function uploadLargeFile(file, url) {
+	service.post(url, { file: file }, { headers: { "Content-Type": "multipart/form-data" } });
+
 	// service.post(url, { file: file }, options)
 	// 	.then((response) => {
 	// 		console.log(response);
